5ccc64286f6c5814bca3adc32d9ef125efcd54a3
Ming Li <mingli434@gmail.com>
2019-07-29 21:58:15 -0400

    login screen layout

--- app/src/main/java/com/gitspark/gitspark/ui/login/LoginActivity.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/login/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.gitspark.gitspark.ui.login
 
 import android.os.Bundle
+import com.gitspark.gitspark.R
 import com.gitspark.gitspark.ui.base.BaseActivity
 
 class LoginActivity : BaseActivity<LoginViewModel>(LoginViewModel::class.java) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
     }
 }
--- app/src/main/java/com/gitspark/gitspark/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/login/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.gitspark.gitspark.ui.login
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel()
+class LoginViewModel @Inject constructor() : ViewModel()
