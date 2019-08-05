52d0ea8833798c708d048584e88e3db83bb34e59
Ming Li <mingli434@gmail.com>
2019-08-04 23:31:01 -0400

    temp icons and bottom navigation

--- app/src/main/java/com/gitspark/gitspark/ui/main/MainActivity.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/main/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.gitspark.gitspark.ui.main
 
 import android.os.Bundle
+import com.gitspark.gitspark.R
 import com.gitspark.gitspark.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
     override fun observeViewModel() {
