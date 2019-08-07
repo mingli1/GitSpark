8d4045a5132b5c3a8d291b5a724a011f011b922d
Ming Li <mingli434@gmail.com>
2019-08-06 23:13:31 -0400

    observe viewmodel in basefragment

--- app/src/main/java/com/gitspark/gitspark/ui/base/BaseFragment.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/base/BaseFragment.kt
@@ -1,8 +1,12 @@
 package com.gitspark.gitspark.ui.base
 
 import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.gitspark.gitspark.extension.observe
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -18,4 +22,19 @@ abstract class BaseFragment<T : BaseViewModel>(private val clazz: Class<T>) : Fr
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.checkInitialized()
+        observeViewModel()
+        observeBaseViewModel()
+    }
+
+    abstract fun observeViewModel()
+
+    private fun observeBaseViewModel() {
+        viewModel.alertAction.observe(viewLifecycleOwner) { showAlert(it) }
+    }
+
+    private fun showAlert(message: String) = Toast.makeText(context, message, LENGTH_SHORT).show()
 }
