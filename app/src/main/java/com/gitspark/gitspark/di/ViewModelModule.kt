5df90fdee664305d32f277232aa442cc379dae87
Ming Li <mingli434@gmail.com>
2019-08-08 01:46:15 -0400

    set up bottom nav to use navigation component

--- app/src/main/java/com/gitspark/gitspark/di/ViewModelModule.kt
+++ app/src/main/java/com/gitspark/gitspark/di/ViewModelModule.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.gitspark.gitspark.ui.base.ViewModelFactory
 import com.gitspark.gitspark.ui.login.LoginViewModel
 import com.gitspark.gitspark.ui.main.MainViewModel
+import com.gitspark.gitspark.ui.main.fragment.*
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -19,6 +20,26 @@ internal abstract class ViewModelModule {
     @Binds @IntoMap
     @ViewModelKey(MainViewModel::class)
     internal abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
+
+    @Binds @IntoMap
+    @ViewModelKey(FeedViewModel::class)
+    internal abstract fun bindFeedViewModel(feedViewModel: FeedViewModel): ViewModel
+
+    @Binds @IntoMap
+    @ViewModelKey(ProfileViewModel::class)
+    internal abstract fun bindProfileViewModel(profileViewModel: ProfileViewModel): ViewModel
+
+    @Binds @IntoMap
+    @ViewModelKey(SearchViewModel::class)
+    internal abstract fun bindSearchViewModel(searchViewModel: SearchViewModel): ViewModel
+
+    @Binds @IntoMap
+    @ViewModelKey(PullRequestsViewModel::class)
+    internal abstract fun bindPullRequestsViewModel(pullRequestsViewModel: PullRequestsViewModel): ViewModel
+
+    @Binds @IntoMap
+    @ViewModelKey(IssuesViewModel::class)
+    internal abstract fun bindIssuesViewModel(issuesViewModel: IssuesViewModel): ViewModel
 
     @Binds
     internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
--- app/src/main/java/com/gitspark/gitspark/ui/main/MainActivity.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/main/MainActivity.kt
@@ -3,27 +3,33 @@ package com.gitspark.gitspark.ui.main
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.gitspark.gitspark.R
 import com.gitspark.gitspark.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java) {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         adjustBottomNavigationText()
-        addBottomNavigationListeners()
+
+        navController = Navigation.findNavController(this, R.id.fragment_nav)
+        bottom_navigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 
     override fun observeViewModel() {
 
-    }
-
-    private fun addBottomNavigationListeners() {
-        bottom_navigation.setOnNavigationItemSelectedListener { menuItem ->
-            true
-        }
     }
 
     // see https://github.com/material-components/material-components-android/issues/139
