9735471e1dfe55d2a11cae85851bbcb3bba7c579
Ming Li <mingli434@gmail.com>
2019-08-12 01:55:13 -0400

    added viewpager for tabs

--- app/src/main/java/com/gitspark/gitspark/ui/main/tab/ProfileFragment.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/main/tab/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.gitspark.gitspark.R
 import com.gitspark.gitspark.ui.base.BaseFragment
+import com.gitspark.gitspark.ui.main.tab.profile.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : BaseFragment<ProfileViewModel>(ProfileViewModel::class.java) {
 
@@ -13,7 +15,23 @@ class ProfileFragment : BaseFragment<ProfileViewModel>(ProfileViewModel::class.j
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setUpTabLayout()
+    }
+
     override fun observeViewModel() {
 
+    }
+
+    private fun setUpTabLayout() {
+        val adapter = ViewPagerAdapter(childFragmentManager).apply {
+            addFragment(OverviewFragment(), getString(R.string.overview_title))
+            addFragment(ReposFragment(), getString(R.string.repos_title))
+            addFragment(ProjectsFragment(), getString(R.string.projects_title))
+            addFragment(StarsFragment(), getString(R.string.stars_title))
+        }
+        viewpager.adapter = adapter
+        tabs.setupWithViewPager(viewpager)
     }
 }
---
+++ app/src/main/java/com/gitspark/gitspark/ui/main/tab/profile/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.gitspark.gitspark.ui.main.tab.profile
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    private val fragmentList = arrayListOf<Fragment>()
+    private val fragmentTitleList = arrayListOf<String>()
+
+    override fun getCount() = fragmentList.size
+
+    override fun getItem(position: Int) = fragmentList[position]
+
+    override fun getPageTitle(position: Int) = fragmentTitleList[position]
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        fragmentTitleList.add(title)
+    }
+}
