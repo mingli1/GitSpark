a82a7512f8b24b315bc7eb1d561f296577b84c09
Ming Li <mingli434@gmail.com>
2019-08-25 13:31:06 -0400

    removed repo from tests

--- app/src/test/java/com/gitspark/gitspark/repository/RepoRepositoryTest.kt
+++ app/src/test/java/com/gitspark/gitspark/repository/RepoRepositoryTest.kt
@@ -1,16 +1,11 @@
 package com.gitspark.gitspark.repository
 
-import com.gitspark.gitspark.api.model.SORT_CREATED
-import com.gitspark.gitspark.api.model.SORT_FULL_NAME
-import com.gitspark.gitspark.api.model.SORT_PUSHED
 import com.gitspark.gitspark.helper.RetrofitHelper
-import com.gitspark.gitspark.helper.TimeHelper
 import com.gitspark.gitspark.model.Page
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.verify
 import io.reactivex.Observable
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.plugins.RxJavaPlugins
@@ -19,15 +14,12 @@ import org.junit.Before
 import org.junit.Test
 
 private const val TOKEN = "Token 123"
-private const val SEARCH = "search"
 
 class RepoRepositoryTest {
 
     private lateinit var realRepository: RepoRepository
     @MockK private lateinit var repoRepository: RepoRepository
     @RelaxedMockK private lateinit var retrofitHelper: RetrofitHelper
-    @RelaxedMockK private lateinit var timeHelper: TimeHelper
-    @RelaxedMockK private lateinit var repoDao: RepoDao
 
     @Before
     fun setup() {
@@ -35,7 +27,7 @@ class RepoRepositoryTest {
         RxJavaPlugins.setInitIoSchedulerHandler { Schedulers.trampoline() }
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
 
-        realRepository = RepoRepository(retrofitHelper, timeHelper, repoDao)
+        realRepository = RepoRepository(retrofitHelper)
     }
 
     @Test
@@ -68,41 +60,5 @@ class RepoRepositoryTest {
                 Observable.just(RepoResult.Failure("failure"))
         val observer = repoRepository.getAuthStarredRepos(token = TOKEN).test()
         observer.assertValue(RepoResult.Failure("failure"))
-    }
-
-    @Test
-    fun shouldGetReposByFullName() {
-        realRepository.getRepos(SEARCH, true, SORT_FULL_NAME)
-        verify { repoDao.getPrivateReposDefaultOrder(any()) }
-
-        realRepository.getRepos(SEARCH, false, SORT_FULL_NAME)
-        verify { repoDao.getAllReposDefaultOrder(any()) }
-    }
-
-    @Test
-    fun shouldGetReposByPushed() {
-        realRepository.getRepos(SEARCH, true, SORT_PUSHED)
-        verify { repoDao.getPrivateReposOrderByPushed(any()) }
-
-        realRepository.getRepos(SEARCH, false, SORT_PUSHED)
-        verify { repoDao.getAllReposOrderByPushed(any()) }
-    }
-
-    @Test
-    fun shouldGetReposByCreated() {
-        realRepository.getRepos(SEARCH, true, SORT_CREATED)
-        verify { repoDao.getPrivateReposOrderByCreated(any()) }
-
-        realRepository.getRepos(SEARCH, false, SORT_CREATED)
-        verify { repoDao.getAllReposOrderByCreated(any()) }
-    }
-
-    @Test
-    fun shouldGetReposByUpdated() {
-        realRepository.getRepos(SEARCH, true, "updated")
-        verify { repoDao.getPrivateReposOrderByUpdated(any()) }
-
-        realRepository.getRepos(SEARCH, false, "updated")
-        verify { repoDao.getAllReposOrderByUpdated(any()) }
     }
 }
