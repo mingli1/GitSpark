09f50f9ac6f07acbda9e9a0da1cab5f25030dad7
Ming Li <mingli434@gmail.com>
2019-08-26 17:30:07 -0400

    created abstract adapter for pagination

---
+++ app/src/main/java/com/gitspark/gitspark/ui/adapter/PaginationAdapter.kt
@@ -0,0 +1,52 @@
+package com.gitspark.gitspark.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gitspark.gitspark.R
+import com.gitspark.gitspark.extension.inflate
+import com.gitspark.gitspark.model.Loading
+
+abstract class PaginationAdapter : RecyclerView.Adapter<PaginationAdapter.ViewHolder>() {
+
+    protected val items = arrayListOf<Pageable>()
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int) = items[position].getViewType()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(parent.inflate(
+            if (viewType == VIEW_TYPE_VIEW) getViewHolderId() else R.layout.loading_view
+        ))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(items[position])
+
+    abstract fun getViewHolderId(): Int
+
+    abstract fun bind(item: Pageable, view: View)
+
+    open fun addInitialItems(items: List<Pageable>, isOnlyPage: Boolean) {
+        if (items.isEmpty()) return
+        this.items.clear()
+
+        this.items.addAll(items)
+        if (!isOnlyPage) this.items.add(Loading)
+        notifyDataSetChanged()
+    }
+
+    open fun addItemsOnLoadingComplete(items: List<Pageable>, isLastPage: Boolean) {
+        if (this.items.lastIndex < 0) return
+        if (this.items.lastOrNull()?.getViewType() != VIEW_TYPE_LOADING) return
+        this.items.removeAt(this.items.lastIndex)
+
+        this.items.addAll(items)
+        if (!isLastPage) this.items.add(Loading)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: Pageable) = bind(item, view)
+    }
+}
