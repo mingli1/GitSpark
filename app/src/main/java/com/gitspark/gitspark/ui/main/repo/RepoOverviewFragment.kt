e997785e22ebcb2063a4edbbc2c70c47d827c16d
Ming Li <mingli434@gmail.com>
2019-09-08 18:25:19 -0400

    added repo overview layout

--- app/src/main/java/com/gitspark/gitspark/ui/main/repo/RepoOverviewFragment.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/main/repo/RepoOverviewFragment.kt
@@ -1,8 +1,17 @@
 package com.gitspark.gitspark.ui.main.repo
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gitspark.gitspark.R
 import com.gitspark.gitspark.ui.base.BaseFragment
 
 class RepoOverviewFragment : BaseFragment<RepoOverviewViewModel>(RepoOverviewViewModel::class.java) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_repo_overview, container, false)
+    }
 
     override fun observeViewModel() {
 
