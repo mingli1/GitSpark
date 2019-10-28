cfa5ae84476116ac110ad12e5ac6041a71f30308
Ming Li <mingli434@gmail.com>
2019-10-27 22:29:20 -0400

    changed backstack on bottom nav

--- app/src/main/java/com/gitspark/gitspark/ui/main/MainActivity.kt
+++ app/src/main/java/com/gitspark/gitspark/ui/main/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.gitspark.gitspark.ui.main
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
 import com.gitspark.gitspark.R
-import com.gitspark.gitspark.extension.isVisible
-import com.gitspark.gitspark.extension.observe
 import com.gitspark.gitspark.extension.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adjustBottomNavigationText()
 
         if (savedInstanceState == null) {
             setUpNavController()
@@ -48,34 +43,5 @@ class MainActivity : AppCompatActivity() {
             containerId = R.id.nav_host_container,
             intent = intent
         )
-        addDestinationListener()
-    }
-
-    private fun addDestinationListener() {
-        navController?.observe(this) {
-            it.addOnDestinationChangedListener { _, dest, args ->
-                bottom_navigation_view.isVisible = when (dest.id) {
-                    R.id.profile_fragment -> args == null
-                    R.id.edit_profile_fragment -> false
-                    R.id.repo_detail_fragment -> false
-                    R.id.repo_code_fragment -> false
-                    R.id.user_list_fragment -> false
-                    R.id.repo_list_fragment -> false
-                    R.id.commit_list_fragment -> false
-                    else -> true
-                }
-            }
-        }
-    }
-
-    // see https://github.com/material-components/material-components-android/issues/139
-    private fun adjustBottomNavigationText() {
-        val menuView = bottom_navigation_view.getChildAt(0) as? ViewGroup
-        menuView?.let { menu ->
-            for (i in 0 until menu.childCount) {
-                val item = menu.getChildAt(i)
-                item.findViewById<View>(R.id.largeLabel).setPadding(0, 0, 0, 0)
-            }
-        }
     }
 }
